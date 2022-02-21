4557fe53693f5eee158e44889de90661b0263a26
Kirill <64753246+Kirill2603@users.noreply.github.com>
2022-02-21 15:44:51 +0300

    add img styles

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import Navbar from "./navbar";
 import style from '../../styles/header.module.scss'
+import Image from "next/image";
+import logo from '../../public/next-logo.png'
 
 const Header = () => {
 	return (
 		<header className={style.header}>
-			<h1>NextApp</h1>
+			<div className={style.logoWrapper}>
+				<Image
+					objectFit={"contain"}
+					layout={"fill"}
+					src={logo}
+					alt={'Logo'}
+					placeholder={"blur"}
+				/>
+			</div>
 			<Navbar />
 		</header>
 	)
