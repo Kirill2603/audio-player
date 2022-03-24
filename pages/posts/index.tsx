9e7ff22a19e8bf900b005d0afefd26697e2487c3
Kirill <killrill2603@gmail.com>
2022-03-24 16:40:40 +0300

    change posts

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,7 +1,8 @@
-import React, {FC} from "react";
+import React, {ChangeEvent, FC, useState} from "react";
 import Head from "next/head";
 import {GetStaticProps} from "next";
 import {PostType} from "../../types";
+import post from "./post";
 
 
 type PostsPropsType = {
@@ -24,20 +25,45 @@ export const getStaticProps: GetStaticProps = async () => {
 }
 
 const Posts: FC<PostsPropsType> = ({posts}) => {
+
+	const [postsCount, setPostCount] = useState<number>(1)
+	const [start, setStart] = useState<number>(0)
+	const [end, setEnd] = useState<number>(10)
+
+	let filteredPosts = posts.slice(start,end)
+
+	const nextPage = () => {
+		setStart(start + 10)
+		setEnd(end + 10)
+		setPostCount(postsCount + 1)
+	}
+	const prevPage = () => {
+		setStart(start - 10)
+		setEnd(end - 10)
+		setPostCount(postsCount - 1)
+	}
+
 	return (
 		<>
 			<Head>
 				<title>Posts</title>
 			</Head>
 			<div>Posts :</div>
+			<div>Page : {postsCount}</div>
 			<ul>
-				{posts && posts.map(({id, title, body}) => (
-					<>
-						<li key={id}>{title}</li>
-						<span>{body}</span>
-					</>
+				{filteredPosts && filteredPosts.map(({id, title, body}) => (
+
+						<li key={id}>{title}
+							<span>{body}</span>
+						</li>
+
+
 				))}
 			</ul>
+			<div>
+				<button disabled={start<=0} onClick={() => prevPage()}>{`<-`}</button>
+				<button disabled={end>=posts.length} onClick={() => nextPage()}>{`->`}</button>
+			</div>
 		</>
 	)
 
